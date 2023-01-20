d3d31aae0929dca336d5d9c96321419d91b219b2
patte <p@tte.io>
2023-01-20 22:00:24 +0100

    fix: cookie SameSite=Lax

--- lib/cookies.ts
+++ lib/cookies.ts
@@ -6,7 +6,7 @@ export function setDocumentCookie(
   let date = new Date()
   date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000)
   const expires = 'expires=' + date.toUTCString()
-  document.cookie = `${cName}=${cValue}; ${expires}; path=/; SameSite=Strict`
+  document.cookie = `${cName}=${cValue}; ${expires}; path=/; SameSite=Lax`
 }
 
 export function getIsDocumentCookieSet(cName: string) {
