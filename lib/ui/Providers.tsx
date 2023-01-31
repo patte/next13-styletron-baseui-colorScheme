patte/next13-styletron-baseui-colorScheme

commit 4e9456b4f175826f67ac18e85db1ea9e2227ea29

fix: colorScheme toggling, styletron overwriting

lib/ui/Providers.tsx
@@ -2,16 +2,12 @@ import { SsrBaseProvider } from './BaseProvider'
 import { SsrColorSchemeProvider } from './colorScheme/server'
 import { SsrStyletronProvider } from './StyletronProvider'
 
-export function Providers({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SsrStyletronProvider>
-      <SsrColorSchemeProvider>
+    <SsrColorSchemeProvider>
+      <SsrStyletronProvider>
         <SsrBaseProvider>{children}</SsrBaseProvider>
-      </SsrColorSchemeProvider>
-    </SsrStyletronProvider>
+      </SsrStyletronProvider>
+    </SsrColorSchemeProvider>
   )
 }

lib/ui/StyletronProvider.tsx
@@ -2,15 +2,19 @@
 
 import { Provider as StyletronProvider } from 'styletron-react'
 import { Client, Server } from 'styletron-engine-atomic'
-import { useState } from 'react'
+import { useMemo } from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
+import { ColorScheme, useColorScheme } from './colorScheme/client'
 
 const ELEMENT_CLASS_NAME = '_styletron_hydrate_'
 
-const createStyletron = () =>
+const createStyletron = ({ colorScheme }: { colorScheme: ColorScheme }) =>
   typeof window === 'undefined'
-    ? new Server()
+    ? new Server({
+        prefix: `${colorScheme}_`,
+      })
     : new Client({
+        prefix: `${colorScheme}_`,
         hydrate: document.getElementsByClassName(
           ELEMENT_CLASS_NAME,
         ) as HTMLCollectionOf<HTMLStyleElement>,
@@ -21,7 +25,12 @@ export function SsrStyletronProvider({
 }: {
   children: React.ReactNode
 }) {
-  const [styletron] = useState(() => createStyletron())
+  const { colorScheme } = useColorScheme()
+
+  const [styletron] = useMemo(
+    () => [createStyletron({ colorScheme })],
+    [colorScheme],
+  )
 
   useServerInsertedHTML(() => {
     // @ts-ignore getSylesheets is not in the type definition

lib/ui/colorScheme/client.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import {
   createContext,
   useCallback,
@@ -14,9 +15,16 @@ export enum ColorScheme {
   dark = 'dark',
 }
 
-export const ColorSchemeContext = createContext({
+type ColorSchemeContext = {
+  colorScheme: ColorScheme
+  toggleColorScheme: () => void
+}
+
+export const ColorSchemeContext = createContext<ColorSchemeContext>({
   colorScheme: ColorScheme.light,
-  toggleColorScheme: () => {},
+  toggleColorScheme: () => {
+    throw new Error('toggleColorScheme used outside of ColorSchemeProvider')
+  },
 })
 
 export function useColorScheme() {
@@ -43,6 +51,7 @@ export function ClientProvider({
   cookieColorSchema: string | null
   cookieName: string
 }) {
+  const router = useRouter()
   const inputColorScheme =
     cleanColorScheme(cookieColorSchema) || cleanColorScheme(headerColorSchema)
 
@@ -57,8 +66,11 @@ export function ClientProvider({
       }
       setDocumentCookie(cookieName, colorScheme)
       setActiveColorScheme(colorScheme)
+      // send the cookie to the server and thus
+      // update the props (header, cookie) for this component
+      router.refresh()
     },
-    [activeColorScheme, cookieName],
+    [activeColorScheme, cookieName, router],
   )
 
   const toggle = useCallback(() => {

commit 73287d426e220b4e90297e9b2ab8a8c89bea08bf

fix(fe): styletron workarounds
fixes: 4e9456b4f175826f67ac18e85db1ea9e2227ea29

lib/ui/Providers.tsx
@@ -4,10 +4,10 @@ import { SsrStyletronProvider } from './StyletronProvider'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SsrColorSchemeProvider>
-      <SsrStyletronProvider>
+    <SsrStyletronProvider>
+      <SsrColorSchemeProvider>
         <SsrBaseProvider>{children}</SsrBaseProvider>
-      </SsrStyletronProvider>
-    </SsrColorSchemeProvider>
+      </SsrColorSchemeProvider>
+    </SsrStyletronProvider>
   )
 }

lib/ui/StyletronProvider.tsx
@@ -1,37 +1,62 @@
 'use client'
 
 import { Provider as StyletronProvider } from 'styletron-react'
-import { Client, Server } from 'styletron-engine-atomic'
+import { Client, Server } from 'styletron-engine-monolithic'
 import { useMemo } from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
-import { ColorScheme, useColorScheme } from './colorScheme/client'
 
-const ELEMENT_CLASS_NAME = '_styletron_hydrate_'
-
-const createStyletron = ({ colorScheme }: { colorScheme: ColorScheme }) =>
-  typeof window === 'undefined'
-    ? new Server({
-        prefix: `${colorScheme}_`,
-      })
-    : new Client({
-        prefix: `${colorScheme}_`,
-        hydrate: document.getElementsByClassName(
-          ELEMENT_CLASS_NAME,
-        ) as HTMLCollectionOf<HTMLStyleElement>,
-      })
+const HYDRATION_ELEMENT_CLASS_NAME = '_styletron_hydrate_'
+const CONTAINER_ELEMENT_ID = '_styletron_container_'
 
 export function SsrStyletronProvider({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const { colorScheme } = useColorScheme()
+  const styletron = useMemo(() => {
+    if (typeof window === 'undefined') {
+      return new Server()
+    } else {
+      // workaround 1:
+      // when this component is rendered, styletron creates new styles
+      // without removing the old ones. This causes issues. We create
+      // a container element and remove the old one if it exists.
+      const oldContainer = document.getElementById(CONTAINER_ELEMENT_ID)
 
-  const [styletron] = useMemo(
-    () => [createStyletron({ colorScheme })],
-    [colorScheme],
-  )
+      const newContainer = document.createElement('style')
+      newContainer.setAttribute('id', CONTAINER_ELEMENT_ID)
+      document.head.appendChild(newContainer)
 
+      // https://github.com/vercel/next.js/issues/44125
+      // workaround 2:
+      // useServerInsertedHTML sometimes (on route push) renders to body
+      // instead of head. We check if there are multiple style elements
+      // and remove the all in the body.
+      const hydrateElements = document.getElementsByClassName(
+        HYDRATION_ELEMENT_CLASS_NAME,
+      ) as HTMLCollectionOf<HTMLStyleElement>
+      if (hydrateElements?.length > 1) {
+        for (let i = 0; i < hydrateElements.length; i++) {
+          if (hydrateElements[i].parentNode === document.body) {
+            hydrateElements[i].remove()
+          }
+        }
+      }
+
+      const newClient = new Client({
+        hydrate: hydrateElements,
+        container: newContainer,
+      })
+
+      if (oldContainer) {
+        oldContainer.remove()
+      }
+
+      return newClient
+    }
+  }, [])
+
+  // see workaround 2
   useServerInsertedHTML(() => {
     // @ts-ignore getSylesheets is not in the type definition
     const stylesheets = styletron.getStylesheets() || []
@@ -39,7 +64,7 @@ export function SsrStyletronProvider({
       <>
         {stylesheets.map((sheet: any, i: number) => (
           <style
-            className={ELEMENT_CLASS_NAME}
+            className={HYDRATION_ELEMENT_CLASS_NAME}
             dangerouslySetInnerHTML={{ __html: sheet.css }}
             media={sheet.attrs.media}
             data-hydrate={sheet.attrs['data-hydrate']}